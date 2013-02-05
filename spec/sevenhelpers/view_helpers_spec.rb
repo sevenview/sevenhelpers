03066439160e094aee83850d8a4130e26bb8473c
Steve Clarke <steve@sevenview.ca>
2013-02-05 11:00:52 -0330

    Add helper to embed Google Analytics tracking code

--- spec/sevenhelpers/view_helpers_spec.rb
+++ spec/sevenhelpers/view_helpers_spec.rb
@@ -1,12 +1,13 @@
 require 'spec_helper'
 
 describe Sevenhelpers::ViewHelpers do
+  let(:helper) { ActionView::Base.new }
+
   it "integrates with ActionView::Base" do
     ActionView::Base.new.should respond_to(:copyright_years)
   end
 
   describe "#copyright_years" do
-    let(:helper) { ActionView::Base.new }
 
     it "returns only the current year if no start_year is specified" do
       helper.copyright_years.should == Time.now.year.to_s
@@ -19,5 +20,13 @@ describe Sevenhelpers::ViewHelpers do
     it "returns the correct range given a start_year" do
       helper.copyright_years(2010).should == "2010-#{Time.now.year.to_s}"
     end
+  end
+
+  describe "#google_analytics_code" do
+
+    it "displays the google tracking code with the tracking id embedded" do
+      helper.google_analytics_code('UA-111111-1').should match /'_setAccount', 'UA-111111-1'/
+    end
+
   end
 end
